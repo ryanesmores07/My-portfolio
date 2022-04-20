19becf253bbd941684e1257290c89617cb8f748a
Ryan Esmores <ryanesmores07@gmail.com>
2022-04-20 23:14:15 +0900

    about me content

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <Wrapper>
       <section className="container">
-        <div className="about-me">
+        <div className="title">
           <h2>About me</h2>
         </div>
         <div className="my-photo">
@@ -16,20 +16,50 @@ const About = () => {
             alt="my-photo"
           />
         </div>
+        <div className="content">
+          <h2>About me</h2>
+          <h3>
+            Hi, I'm Ryan Esmores, a self-taught front-end web developer from the
+            Philippines.
+          </h3>
+          <h3>
+            Enthusiastic front-end web developer with a "can-do" attitude. I
+            love learning new things whether its a new skill, or a hobby, and I
+            always strive to be a better version of myself.
+          </h3>
+        </div>
       </section>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.main`
-  .container {
-    padding: 3rem 5rem;
+  width: 100%;
 
-    .about-me {
-      margin-bottom: 3rem;
+  .container {
+    width: 80%;
+    /* padding: 3rem; */
+    margin: 0 auto;
+
+    .title {
+      margin: 0rem 0rem 5rem;
     }
 
-    .photo {
+    .my-photo {
+      padding: 1rem;
+      margin-bottom: 5rem;
+      .photo {
+        object-fit: contain;
+        /* max-width: 30%; */
+      }
+    }
+
+    .content {
+      h2 {
+        font-weight: 300;
+        font-family: var(--ff-primary);
+        font-size: 2.5rem;
+      }
     }
   }
 `
