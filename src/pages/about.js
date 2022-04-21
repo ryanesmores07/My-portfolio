edffdf5348bb4988adeff149bcacc15406d72081
Ryan Esmores <ryanesmores07@gmail.com>
2022-04-21 22:50:05 +0900

    worked with about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,14 +19,25 @@ const About = () => {
         <div className="content">
           <h2>About me</h2>
           <h3>
-            Hi, I'm Ryan Esmores, a self-taught front-end web developer from the
-            Philippines.
+            <strong>Hi, I'm Ryan Esmores</strong>, a self-taught front-end web
+            developer from the Philippines.
           </h3>
-          <h3>
+          <p>
             Enthusiastic front-end web developer with a "can-do" attitude. I
             love learning new things whether its a new skill, or a hobby, and I
             always strive to be a better version of myself.
-          </h3>
+          </p>
+          <div className="blockquote-container">
+            <div className="blockquote">
+              <q>You can do anything you set your mind to, man.</q>
+              <span> - Eminem</span>
+            </div>
+          </div>
+          <p>
+            I may lack the experience of working on a real world project in the
+            tech world, but I can guarantee that I pick up quickly, and my
+            enthusiasism, and eagerness to learn will make up for it.
+          </p>
         </div>
       </section>
     </Wrapper>
@@ -37,7 +48,7 @@ const Wrapper = styled.main`
   width: 100%;
 
   .container {
-    width: 80%;
+    max-width: 80%;
     /* padding: 3rem; */
     margin: 0 auto;
 
@@ -59,6 +70,39 @@ const Wrapper = styled.main`
         font-weight: 300;
         font-family: var(--ff-primary);
         font-size: 2.5rem;
+        margin-bottom: 3rem;
+      }
+
+      h3 {
+        font-size: 2.5rem;
+        margin-bottom: 3rem;
+        line-height: 1.5;
+      }
+
+      p {
+        font-size: 1.5rem;
+        margin-bottom: 2.5rem;
+      }
+      
+    }
+
+    .blockquote-container {
+      display: flex;
+      margin-left: 3rem;
+      margin-bottom: 2.5rem;
+      justify-content: flex-start;
+      /* align-items: center; */
+        width: 70%;
+        border-left: 0.5rem solid steelblue;
+        padding-left: 1rem;
+
+        q {
+          font-size: 2rem;
+          line-height: 1;
+        }
+        span {
+        font-size: 2.5rem;
+      }
       }
     }
   }
