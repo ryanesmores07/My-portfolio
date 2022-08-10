10ab977808404f57dbf6148dc2c780eae3f556ef
Ryan Esmores <ryanesmores07@gmail.com>
2022-08-10 23:26:08 +0900

    restored contact page

---
+++ src/components/Sidebar-copy.js
@@ -0,0 +1,87 @@
+// import React, { useContext } from "react"
+// import styled from "styled-components"
+// import { GatsbyContext } from "../context/context"
+// import { Link } from "gatsby"
+// import { motion, AnimatePresence } from "framer-motion"
+
+// const Sidebar = () => {
+//   const { links, hideSidebar, isSidebarOpen } = useContext(GatsbyContext)
+//   console.log(links)
+
+//   return (
+//     <AnimatePresence>
+//       <Wrapper>
+//         <section className="sidebar-container">
+//           <motion.div
+//             exit={{ opacity: 0 }}
+//             className={`${!isSidebarOpen ? "sidebar" : "sidebar show"}`}
+//           >
+//             {links.map((link, index) => {
+//               return (
+//                 <ul onClick={() => hideSidebar()}>
+//                   <Link to={link.url} className="yo">
+//                     <li>{link.text}</li>
+//                   </Link>
+//                 </ul>
+//               )
+//             })}
+//           </motion.div>
+//         </section>
+//       </Wrapper>
+//     </AnimatePresence>
+//   )
+// }
+
+// const Wrapper = styled.aside`
+//   .sidebar-container {
+//     overflow: hidden;
+//     .sidebar {
+//       position: fixed;
+//       top: 0rem;
+//       right: -100%;
+
+//       /* transform: translateX(-10rem); */
+
+//       height: 100%;
+//       padding: 7rem 10rem 7rem 7rem;
+
+//       background-color: rgba(255, 255, 255, 0.9);
+//       transition: all 0.5s ease-in-out;
+
+//       li {
+//         color: var(--clr-black);
+//         /* background-color: red; */
+//         line-height: 2;
+//         font-size: 3.5rem;
+//         font-weight: 300;
+
+//         -webkit-text-stroke: 1.2px black;
+
+//         &:hover {
+//           color: black;
+//           transition: var(--transition);
+//         }
+//       }
+//     }
+//     .sidebar.show {
+//       /* transform: translateX(0); */
+//       right: 0;
+//       transition: all 0.5s ease-in-out;
+//     }
+//   }
+
+//   @media (min-width: 768px) {
+//     .sidebar-container {
+//       .sidebar {
+//         li {
+//           color: var(--clr-white);
+//           font-size: 8rem;
+//           font-weight: 400;
+//           line-height: 1.5;
+//         }
+//       }
+//     }
+//   }
+// `
+
+// export default Sidebar
---
+++ src/pages/contact.js
@@ -0,0 +1,175 @@
+import React from "react"
+import styled from "styled-components"
+import socials from "../constants/socials"
+import { CgArrowsExpandUpRight } from "react-icons/cg"
+
+const Contact = () => {
+  return (
+    <Wrapper>
+      <section className="container">
+        {/* <h2>Contact</h2> */}
+        <div className="title">
+          <h1>get in touch</h1>
+        </div>
+        <div className="content">
+          <p>
+            What is your challenge? I can help you with your website front-end
+            development needs and make it fully responsive with the design and
+            style you like!
+          </p>
+        </div>
+        <div className="socials">
+          <a href="#">
+            <h3>ryanesmores07@gmail.com</h3>
+          </a>
+          <a href="#">
+            <h3 style={{ marginBottom: "6rem" }}>+81 80-7575-0507</h3>
+          </a>
+          {socials.map((social, index) => {
+            return (
+              <div className="social-container">
+                <a href={social.url} className="flex-container">
+                  <div className="social-text">{social.text}</div>
+                  <CgArrowsExpandUpRight className="arrow-icon" />
+                </a>
+
+                <div className="line"></div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  width: 100%;
+  
+
+  .container {
+    margin: 0 auto;
+    max-width: 80%;
+    /* padding: 0rem rem; */
+
+    h1 {
+        font-size: 5.5rem;
+        font-weight: 300;
+        letter-spacing: -2px;
+        margin-bottom: 10rem;
+    }
+
+    h2 {
+        font-weight: 200;
+        font-size: 1.7rem;
+    }
+
+    
+
+    a, h3 {
+        font-weight: 400;
+        font-size: 2rem;
+        color: var(--clr-black);
+        margin-bottom: 2rem;
+        font-family: var(--ff-primary);
+    }
+
+    
+    a { 
+        /* font-style: var(--ff-primary); */
+        font-weight: 200;
+    }
+
+    p {
+        font-size: 2rem;
+        margin-bottom: 10rem;
+    }
+
+
+    .social-container{
+
+      .flex-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0;
+
+         .social-text {
+        margin: 0 1rem;
+        }
+
+        .arrow-icon {
+        font-size: 2rem;
+        margin-right: 1rem;
+        }
+      }
+      
+      .flex-container:hover, .flex-container:focus {
+       
+        
+        .social-text {
+          transform: translateX(.3rem);
+          transition: all 0.1s linear;
+          color: steelblue;
+        }
+      }
+     
+
+     
+      
+      .line {
+      height: .1rem;
+      width: 100%;
+      background: var(--clr-black);
+      opacity: .2;
+      margin: .7rem 0;
+      
+      }
+    }
+    
+    @media (min-width: 450px) {
+      h1 {
+        font-size: 7rem;
+      }
+    }
+
+    @media (min-width: 768px) {
+      h1 {
+        font-size: 13rem;
+      }
+
+      p {
+        max-width: 46ch;
+      }
+    }
+
+    @media (min-width: 1170px) {
+      max-width: 95%;
+      h1 {
+        font-size: 17rem;
+      }
+
+      p {
+        margin: 0 auto;
+        /* width: 80%; */
+        max-width: 75ch;
+        padding: 0rem 8rem;
+        margin-bottom: 10rem;
+        font-size: 2.5rem;
+      }
+
+      .socials {
+        h3 {
+          font-size: 4rem;
+        }
+      }
+
+      .social-container {
+        a {
+          font-size: 2.5rem;
+        }
+      }
+    }
+`
+
+export default Contact
